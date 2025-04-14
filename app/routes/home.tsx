17e7fa781c01a004c8bfd1027190f73f61df564d
cjenaro <jen.calvineo@gmail.com>
2025-04-14 19:55:34 -0300

    fix: unused import

--- app/routes/home.tsx
+++ app/routes/home.tsx
@@ -1,5 +1,5 @@
 import { useLoaderData } from "react-router";
-import { data, useFetcher, useNavigate } from "react-router";
+import { data, useFetcher } from "react-router";
 import type { Route } from "./+types/home";
 import { useState, useEffect, useRef } from "react";
 import type { House } from "./houses";
