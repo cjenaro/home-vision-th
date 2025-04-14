2cc81b2aad14166e83d96ff24bd1eb189ba4dea6
cjenaro <jen.calvineo@gmail.com>
2025-04-14 11:52:27 -0300

    feat: favicon

--- app/root.tsx
+++ app/root.tsx
@@ -21,6 +21,8 @@ export const links: Route.LinksFunction = () => [
     rel: "stylesheet",
     href: "https://fonts.googleapis.com/css2?family=Inter:ital,opsz,wght@0,14..32,100..900;1,14..32,100..900&display=swap",
   },
+  { rel: "icon", type: "image/png", href: "favicon-32x32.png", sizes: "32x32" },
+  { rel: "icon", type: "image/png", href: "favicon-16x16.png", sizes: "16x16" },
 ];
 
 export function Layout({ children }: { children: React.ReactNode }) {
